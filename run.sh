69cb0a8957e527745510bb979203b98877a1b8aa
gomesuit <gomesuit@gmail.com>
2016-05-08 03:09:35 +0900

    Correct code

--- run.sh
+++ run.sh
@@ -7,7 +7,9 @@ echo "$WERCKER_IDCF_S3SYNC_HOST_BUCKET"
 
 s3cfg="$HOME/.s3cfg"
 
-echo "access_key=$WERCKER_IDCF_S3SYNC_ACCESS_KEY" >> "$s3cfg"
-echo "secret_key=$WERCKER_IDCF_S3SYNC_SECRET_KEY" >> "$s3cfg"
-echo "host_bucket=$WERCKER_IDCF_S3SYNC_HOST_BASE" >> "$s3cfg"
-echo "access_key=$WERCKER_IDCF_S3SYNC_HOST_BUCKET" >> "$s3cfg"
+{
+  echo "access_key=$WERCKER_IDCF_S3SYNC_ACCESS_KEY"
+  echo "secret_key=$WERCKER_IDCF_S3SYNC_SECRET_KEY"
+  echo "host_bucket=$WERCKER_IDCF_S3SYNC_HOST_BASE"
+  echo "access_key=$WERCKER_IDCF_S3SYNC_HOST_BUCKET"
+} >> "$s3cfg"
