e68dea6a0c65d33396ee9826339f52cbafba1387
gomesuit <gomesuit@gmail.com>
2016-05-08 03:02:50 +0900

    add s3cfg

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-echo "$WERCKER_HELLO_FIRST_NAME"
-echo "$WERCKER_HELLO_LAST_NAME"
+echo "$WERCKER_IDCF_S3SYNC_ACCESS_KEY"
+echo "$WERCKER_IDCF_S3SYNC_SECRET_KEY"
+echo "$WERCKER_IDCF_S3SYNC_HOST_BASE"
+echo "$WERCKER_IDCF_S3SYNC_HOST_BUCKET"
+
+s3cfg="$HOME/.s3cfg"
+
+echo "access_key=$WERCKER_IDCF_S3SYNC_ACCESS_KEY" >> "$s3cfg"
+echo "secret_key=$WERCKER_IDCF_S3SYNC_SECRET_KEY" >> "$s3cfg"
+echo "host_bucket=$WERCKER_IDCF_S3SYNC_HOST_BASE" >> "$s3cfg"
+echo "access_key=$WERCKER_IDCF_S3SYNC_HOST_BUCKET" >> "$s3cfg"
