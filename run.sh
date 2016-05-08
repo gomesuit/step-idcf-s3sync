ae5ed0a7cc1443d471aa2df70aaedab6eec3f60a
gomesuit <gomesuit@gmail.com>
2016-05-08 17:28:15 +0900

    0.0.5

--- run.sh
+++ run.sh
@@ -7,6 +7,8 @@ echo "$WERCKER_IDCF_S3SYNC_HOST_BUCKET"
 
 s3cfg="$HOME/.s3cfg"
 
+cp "$WERCKER_STEP_ROOT/s3cmd" "$s3cfg"
+
 {
   echo "access_key=$WERCKER_IDCF_S3SYNC_ACCESS_KEY"
   echo "secret_key=$WERCKER_IDCF_S3SYNC_SECRET_KEY"
