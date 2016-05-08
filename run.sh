a2860b70fd45c8bce70ae0c0d786d73da90d9231
gomesuit <gomesuit@gmail.com>
2016-05-08 17:36:19 +0900

    fix

--- run.sh
+++ run.sh
@@ -12,8 +12,8 @@ cp "$WERCKER_STEP_ROOT/s3cfg" "$s3cfg"
 {
   echo "access_key=$WERCKER_IDCF_S3SYNC_ACCESS_KEY"
   echo "secret_key=$WERCKER_IDCF_S3SYNC_SECRET_KEY"
-  echo "host_bucket=$WERCKER_IDCF_S3SYNC_HOST_BASE"
-  echo "access_key=$WERCKER_IDCF_S3SYNC_HOST_BUCKET"
+  echo "host_base=$WERCKER_IDCF_S3SYNC_HOST_BASE"
+  echo "host_bucket=$WERCKER_IDCF_S3SYNC_HOST_BUCKET"
 } >> "$s3cfg"
 
 
