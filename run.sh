3649c3e498ffd9033583bc93e5dc20fb364514b3
gomesuit <gomesuit@gmail.com>
2016-05-08 17:23:38 +0900

    cat "$s3cfg"

--- run.sh
+++ run.sh
@@ -15,10 +15,8 @@ s3cfg="$HOME/.s3cfg"
 } >> "$s3cfg"
 
 
+cat "$s3cfg"
+
 source_dir="$WERCKER_ROOT/$WERCKER_IDCF_S3SYNC_SOURCE_DIR"
-
-#"$WERCKER_STEP_ROOT/s3cmd" sync --verbose ./ "$WERCKER_S3SYNC_BUCKET_URL"
-
-#"$WERCKER_STEP_ROOT/s3cmd" put -r --verbose "$source_dir" s3://gomesuit/test/
 
 "$WERCKER_STEP_ROOT/s3cmd" put -r --verbose "$source_dir" "$WERCKER_IDCF_S3SYNC_BUCKET_URL"
