f58057e39dcb492e87e21b2ed638659527ac1285
gomesuit <gomesuit@gmail.com>
2016-05-08 15:58:32 +0900

    put

--- run.sh
+++ run.sh
@@ -13,3 +13,12 @@ s3cfg="$HOME/.s3cfg"
   echo "host_bucket=$WERCKER_IDCF_S3SYNC_HOST_BASE"
   echo "access_key=$WERCKER_IDCF_S3SYNC_HOST_BUCKET"
 } >> "$s3cfg"
+
+
+source_dir="$WERCKER_ROOT/$WERCKER_IDCF_S3SYNC_SOURCE_DIR"
+
+#"$WERCKER_STEP_ROOT/s3cmd" sync --verbose ./ "$WERCKER_S3SYNC_BUCKET_URL"
+
+#"$WERCKER_STEP_ROOT/s3cmd" put -r --verbose "$source_dir" s3://gomesuit/test/
+
+"$WERCKER_STEP_ROOT/s3cmd" put -r --verbose "$source_dir" "$WERCKER_S3SYNC_BUCKET_URL"
